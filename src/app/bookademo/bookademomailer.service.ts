837eada797430113c82fffab31dcfa8aa36b99bc
Ravi Shashtri <rnagunuri@intelexsystemsinc.com>
2020-10-27 11:13:24 +0530

    APi path change

--- src/app/bookademo/bookademomailer.service.ts
+++ src/app/bookademo/bookademomailer.service.ts
@@ -6,8 +6,8 @@ import "rxjs/add/operator/map";
 @Injectable()
 export class BookademomailerService {
   // apiPath: any = 'http://localhost:3200/';
-  apiPath: any = 'http://52.25.21.10:3200/';
-  // apiPath: any = 'https://' + window.location.hostname + ':8443/';
+  // apiPath: any = 'http://52.25.21.10:3200/';
+  apiPath: any = 'https://www.revolutiones.com:8443/';
   constructor(private _http: Http) { }
   emailService(newvalue: any) {
     const headers = new Headers();
